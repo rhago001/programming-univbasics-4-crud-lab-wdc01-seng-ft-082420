0045004893894d55764e7e12f058950397d4d57a
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-30 07:56:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    array().push ("ff") 
-  
+     array.push( element)
+  return array
+
 end
 
 def add_element_to_start_of_array(array, element)
